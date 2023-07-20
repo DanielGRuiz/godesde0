07390d2be9bd38963bd8ff628fe9b5b250433062
Daniel Ruiz <danielgerardoruiz@gmail.com>
2023-07-20 15:26:24 -0400

    Siguiente commit

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/DanielGRuiz/godesdeo/variables"
 )
 
 func main() {
-	variables.MuestroEntero()
+	estado, texto := variables.ConvierteATexto(1577)
+
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
