7c02c7164a3aec179ac341d9455008d7afa9286d
Daniel Ruiz <danielgerardoruiz@gmail.com>
2023-07-26 15:35:00 -0400

    Siguiente commit

--- arreglos_slices/slice.go
+++ arreglos_slices/slice.go
@@ -4,5 +4,13 @@ import "fmt"
 
 func Capacidad() {
 	arreglo1 := make([]int, 10, 15)
-	fmt.Printf("Tamaño $d, capacidad $d", len(arreglo1), cap(arreglo1))
+	fmt.Printf("arreglo1 => Tamaño %d, capacidad %d", len(arreglo1), cap(arreglo1))
+
+	numeros := make([]int, 0, 0)
+	fmt.Printf("numero => Tamaño %d, capacidad %d", len(numeros), cap(numeros))
+
+	for i := 0; i < 10000000; i = 10000 {
+		numeros = append(numeros, i)
+		fmt.Printf("\n numero => Tamaño %d, capacidad %d", len(numeros), cap(numeros))
+	}
 }
